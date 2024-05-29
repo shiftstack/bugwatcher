2062940a97778561bb502a44d7e7fe45d7b7fec8
Stephen Finucane <stephenfin@redhat.com>
2024-05-29 11:53:29 +0100

    pretriage: Dump response body on error
    Unless we encounter e.g. transport issues, we should have a response
    body that contains more useful information than the error message alone.
    
    Signed-off-by: Stephen Finucane <stephenfin@redhat.com>

--- cmd/pretriage/assign.go
+++ cmd/pretriage/assign.go
@@ -10,17 +10,23 @@ import (
 
 func assign(jiraClient *jira.Client, issue jira.Issue, assignee TeamMember) error {
 	res, err := jiraClient.Issue.UpdateAssignee(issue.ID, &jira.User{Name: assignee.JiraName})
-	if err != nil {
+	if err != nil && res == nil {
+		// we only error out early if there's no response to work with
 		return fmt.Errorf("error while assigning bug %s: %w", issue.Key, err)
 	}
 
-	io.Copy(io.Discard, res.Body)
-	res.Body.Close()
+	var body string
+	if res != nil {
+		// we don't check errors since this is best effort
+		bodyBytes, _ := io.ReadAll(res.Body)
+		body = string(bodyBytes)
+		res.Body.Close()
+	}
 
 	switch res.StatusCode {
 	case http.StatusOK, http.StatusNoContent, http.StatusAccepted:
 	default:
-		return fmt.Errorf("unexpected status code %q from Jira while assigning bug %s: %w", res.Status, issue.Key, err)
+		return fmt.Errorf("unexpected status code %q from Jira while assigning bug %s: err=%w body=%s", res.Status, issue.Key, err, body)
 	}
 
 	return nil
